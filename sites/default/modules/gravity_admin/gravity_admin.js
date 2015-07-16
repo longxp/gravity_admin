068af882bb1939dcb9b9af957e6f35cde9b1f460
LongNH <longktpm1k5@gmail.com>
2015-07-16 14:01:52 +0700

    push view-admin

--- sites/default/modules/gravity_admin/gravity_admin.js
+++ sites/default/modules/gravity_admin/gravity_admin.js
@@ -3,10 +3,6 @@
         attach: function () { //start code
             var $root = $('body');
 
-            var $payment_content = $('#block-views-payment-tab-block');
-            var a1 = $payment_content.clone();
-            $("#payment-content").html(a1);
-
 
 
 
